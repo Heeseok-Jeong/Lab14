0940b528d0d7665448e9a908c3d8ff1ce9ce75e8
Heeseok-Jeong <dldydldy@naver.com>
2018-05-03 01:05:23 +0900

    complete prob2 except comments

--- src/main/java/edu/handong/csee/java/lab14/prob2/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob2/Main.java
@@ -1,10 +1,10 @@
 package edu.handong.csee.java.lab14.prob2;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		In_exception obj = new In_exception();
+		obj.error_det();
 	}
 
 }

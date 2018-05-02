20f20eca3a46019d77606d97d3cba3c89c1a3b03
Heeseok-Jeong <dldydldy@naver.com>
2018-05-02 23:49:02 +0900

    complete prob1 except comments

--- src/main/java/edu/handong/csee/java/lab14/prob1/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob1/Main.java
@@ -1,10 +1,23 @@
 package edu.handong.csee.java.lab14.prob1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int limit, speed;
+		Scanner keyboard = new Scanner(System.in);
+		Random rand = new Random();
+		
+		System.out.print("Set the speed limit, officer : ");
+		limit = keyboard.nextInt();
+		speed = rand.nextInt(101);
+		
+		SpeedLimit lim = new SpeedLimit(limit, speed);
+		lim.warnSpeedLimit();
+		
+		keyboard.close();
 	}
 
 }
--- src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimit.java
+++ src/main/java/edu/handong/csee/java/lab14/prob1/SpeedLimit.java
@@ -1,5 +1,23 @@
 package edu.handong.csee.java.lab14.prob1;
 
 public class SpeedLimit {
+	private int limit, speed;
+	
+	public SpeedLimit(int limit, int speed) {
+		this.limit = limit;
+		this.speed = speed;
+	}
+	
+	public void warnSpeedLimit() {
+		try {
+			if(speed > limit) 
+				throw new Exception ("Speed Limit " + limit + "km exceeded!\nYou are being fined.");	
+			System.out.println("You are a law abiding citizen!");
+			}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Your current speed : " + speed + "km");	
+	}
 
 }

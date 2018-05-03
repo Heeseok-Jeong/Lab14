0670c6066a5362f83882e6e97205c8d80eb4c74a
Heeseok-Jeong <dldydldy@naver.com>
2018-05-03 13:14:30 +0900

    complete prob4 except comments

--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -1,5 +1,11 @@
 package edu.handong.csee.java.lab14.prob4;
 
 public class Boundary {
-
+	private int[] arr = new int[5];
+	private int count = 0;
+	
+	public void recieve(int num) throws ArrayIndexOutOfBoundsException {
+		arr[count++] = num;
+		System.out.printf("arr[%d] <- %d\n", count-1, num);
+	}
 }
--- src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
@@ -1,10 +1,27 @@
 package edu.handong.csee.java.lab14.prob4;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num;
+		Scanner keyboard = new Scanner(System.in);
+		Boundary bound = new Boundary();
+		boolean repeat = true;
+		
+		while(repeat) {
+			try {
+				System.out.println("Enter an Integer : ");
+				num = keyboard.nextInt();
+				bound.recieve(num);
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("Invalid array index access!");
+				repeat = false;
+			}
+		}
+		keyboard.close();
 	}
 
 }

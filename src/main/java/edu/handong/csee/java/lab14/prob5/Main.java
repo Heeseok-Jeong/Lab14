9b08cd20e793f613627ae154419173dcba0020b6
Heeseok-Jeong <dldydldy@naver.com>
2018-05-05 15:02:12 +0900

    complete to write comments prob5

--- src/main/java/edu/handong/csee/java/lab14/prob5/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob5/Main.java
@@ -1,29 +1,29 @@
 package edu.handong.csee.java.lab14.prob5;
 
-import java.util.Scanner;
+import java.util.Scanner; //call a Scanner class to use
 
-public class Main {
-	private static String str;
+public class Main { //the Main class
+	private static String str; //declare a String type static variable str
 	
-	public static void main(String[] args) {
-		try {
-			Scanner keyboard = new Scanner(System.in);
-			System.out.println("Input string : ");
-			str = keyboard.nextLine();
-			Main.myTest(str);
-			keyboard.close();
+	public static void main(String[] args) { //the main method 
+		try { //do a below contents
+			Scanner keyboard = new Scanner(System.in); //instantiate Scanner class
+			System.out.println("Input string : "); //print some message to get str
+			str = keyboard.nextLine(); //str value gets from user
+			myTest(str); //call a method myTest to check str is "null"
+			keyboard.close(); //close keyboard
 		}
-		catch(MyException mae) {
-			System.out.println("Inside catch block : " + mae.toString());
+		catch(MyException mae) { //if any error which is in MyException which is subclass of Exception class exists
+			System.out.println("Inside catch block : " + mae.toString()); //print about that error situation
 		}
 		
 	}
 
-	public static void myTest(String str) throws MyException{
-		if(str.equals("null"))
-			throw new MyException("String val is null");
+	public static void myTest(String str) throws MyException{ //this static method checks str is "null" or not print str
+		if(str.equals("null")) //if str is "null"
+			throw new MyException("String val is null"); //notice that situation
 		else
-			System.out.println("String val is : " + str);
+			System.out.println("String val is : " + str); //or not, prints about str
 	}
 	
 }
--- src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
+++ src/main/java/edu/handong/csee/java/lab14/prob5/MyException.java
@@ -1,22 +1,18 @@
 package edu.handong.csee.java.lab14.prob5;
 
-public class MyException extends Exception{
-	private String mStr;
+public class MyException extends Exception{ //the class MyException which is subclass of Exception class
+	private String mStr; //declare instance variable mStr
 	
-	public MyException( ) {
-		super();
+	public MyException(String str) { //constructor which has a String type parameter
+		super(str); //also give this parameter to Exception constructor
+		mStr = str; //mStr value is parameter value
 	}
 	
-	public MyException(String str) {
-		super(str);
-		mStr = str;
+	public MyException(Throwable cause) { //constructor which has a Throwable type parameter
+		super(cause); //also give this parameter to Exception constructor
 	}
 	
-	public MyException(Throwable cause) {
-		super(cause);
-	}
-	
-	public String toString() {
-		return mStr;
+	public String toString() { //this method returns mStr
+		return mStr; //returns mStr
 	}
 }

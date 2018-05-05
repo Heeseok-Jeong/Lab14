f203b3bc5968db8d60a9fae444b9e650d328aecd
Heeseok-Jeong <dldydldy@naver.com>
2018-05-05 15:37:59 +0900

    complete to write comments prob6

--- src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/CustomID.java
@@ -1,41 +1,41 @@
 package edu.handong.csee.java.lab14.prob6;
 
-public class CustomID {
-	String name;
-	String[] race = {"Vulcan", "Romulan", "Klingons"};
-	int age, step = 0;
+public class CustomID { //the class CustomID
+	private String name; //declare String type instance variable name
+	private String[] race = {"Vulcan", "Romulan", "Klingons"}; //initialize String type array race
+	int age, step = 0; //declare instance variable age and initialize step value 0
 	
-	public void setName(String name) throws Exception {
-		if(name.length() < 5) 
-			throw new Exception("Your name is short! Try again!");
-		else {
-			System.out.println("Name is valid\nName : " + name);
-			step++;
+	public void setName(String name) throws Exception { //this method checks name's length is smaller than 5 and set a name and ready to go to next step
+		if(name.length() < 5)  //if name's length is smaller than 5
+			throw new Exception("Your name is short! Try again!"); //notice about this situation
+		else { //or not
+			System.out.println("Name is valid\nName : " + name); //prints about name
+			step++; //plus one step for next step
 		}
 	}
 	
-	public void setAge(int age) throws Exception {
-		if(age < 18) 
-			throw new Exception("You are too young! Try again!");
-		else {
-			System.out.println("Age is valid\nAge : " + age);
-			step++;
+	public void setAge(int age) throws Exception { //this method checks age's value is smaller than 18 and set an age and ready to go to next step
+		if(age < 18)  //if age value is smaller than 18
+			throw new Exception("You are too young! Try again!"); //notice about this situation
+		else { //or not
+			System.out.println("Age is valid\nAge : " + age); //print about age
+			step++; //plus one step for next step
 		}
 	}
 	
-	public void setRace(String str) throws Exception {
-		for(int i = 0; i < race.length; i++) {
-			if(race[i].equals(str)) {
-				System.out.println("Race is valid\nRace : " + race[i]);
-				step = 0;
-				break;
+	public void setRace(String str) throws Exception { //this method checks and print str value includes race and ready to go to setName step
+		for(int i = 0; i < race.length; i++) { //repeat race's length times
+			if(race[i].equals(str)) { //if race[i] value is same str value
+				System.out.println("Race is valid\nRace : " + str); //print about str
+				step = 0; //step is 0 for setName method
+				break; //stop looping
 			}
-			throw new Exception("Human! Try again.");
+			throw new Exception("Human! Try again."); //if all race[i] value isn't same str value notice about this situation
 		}
 	}
 	
-	public int getStep() {
-		return step;
+	public int getStep() { //this method returns step
+		return step; //returns step
 	}
 	
 
--- src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob6/Main.java
@@ -1,36 +1,36 @@
 package edu.handong.csee.java.lab14.prob6;
 
-import java.util.Scanner;
+import java.util.Scanner; //call a Scanner class to use
 
-public class Main {
+public class Main { //the Main class
 
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		CustomID obj = new CustomID();
-		String name, race;
-		int age;
-		boolean repeat = true;
+	public static void main(String[] args) { //the main method
+		Scanner keyboard = new Scanner(System.in); //instantiate Scanner class
+		CustomID obj = new CustomID(); //instantiate CustomID class
+		String name, race; //declare String type instance variable name and race
+		int age; //declare instance variable age
+		boolean repeat = true; //initialize boolean type instance variable repeat with true
 		
-		while(repeat) {
-			try {
-				if(obj.getStep() == 0) {
-					System.out.print("Enter your name : ");
-					name = keyboard.next();
-					obj.setName(name);
+		while(repeat) { //looping while repeat value is true
+			try { //do a below contents
+				if(obj.getStep() == 0) { //if step value is 0
+					System.out.print("Enter your name : "); //print some message to get name
+					name = keyboard.next(); //name value gets from user
+					obj.setName(name); //call a method setName with name
 				}
-				else if(obj.getStep() == 1) {
-					System.out.print("Enter your age : ");
-					age = keyboard.nextInt();
-					obj.setAge(age);
+				else if(obj.getStep() == 1) { //or if step value is 1
+					System.out.print("Enter your age : "); //print some message to get age
+					age = keyboard.nextInt(); //age value gets from user
+					obj.setAge(age); //call a method setAge with age
 					}
-				else if(obj.getStep() == 2) {
-					System.out.print("Enter your race : ");
-					race = keyboard.next();
-					obj.setRace(race);
+				else if(obj.getStep() == 2) { //or if step value is 2
+					System.out.print("Enter your race : "); //print some message to get race
+					race = keyboard.next(); //race value gets from user
+					obj.setRace(race); //call a method setRace with race
 				}
 			}
-			catch(Exception e) {
-				System.out.println(e);
+			catch(Exception e) { //if any error which is in Exception exists
+				System.out.println(e); //print about that situation
 			}
 		}
 		keyboard.close();

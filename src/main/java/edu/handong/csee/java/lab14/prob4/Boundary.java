05c65b781c67f675157eb1bee7b115c3ccedc7ca
Heeseok-Jeong <dldydldy@naver.com>
2018-05-05 14:15:46 +0900

    complete to write comments prob4

--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -1,11 +1,11 @@
 package edu.handong.csee.java.lab14.prob4;
 
-public class Boundary {
-	private int[] arr = new int[5];
-	private int count = 0;
+public class Boundary { //the class Boundary
+	private int[] arr = new int[5]; //declare instance variable arr, int type an array size 5
+	private int count = 0; //initialize instance variable count
 	
-	public void recieve(int num) throws ArrayIndexOutOfBoundsException {
-		arr[count++] = num;
-		System.out.printf("arr[%d] <- %d\n", count-1, num);
+	public void recieve(int num) { //this method set and print array arr value 
+		arr[count] = num; //arr[count] value is num value
+		System.out.printf("arr[%d] <- %d\n", count++, num); //print about arr[count] value and count plus one
 	}
 }
--- src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
@@ -1,27 +1,27 @@
 package edu.handong.csee.java.lab14.prob4;
 
-import java.util.Scanner;
+import java.util.Scanner; //call a Scanner class to use
 
-public class Main {
+public class Main { //the Main class
 
-	public static void main(String[] args) {
-		int num;
-		Scanner keyboard = new Scanner(System.in);
-		Boundary bound = new Boundary();
-		boolean repeat = true;
+	public static void main(String[] args) { //the main method
+		int num; //declare a local variable num
+		boolean repeat = true; //initialize local variable repeat boolean type 
+		Scanner keyboard = new Scanner(System.in); //instantiate Scanner class
+		Boundary bound = new Boundary(); //instantiate Boundary class
 		
-		while(repeat) {
-			try {
-				System.out.println("Enter an Integer : ");
-				num = keyboard.nextInt();
-				bound.recieve(num);
+		while(repeat) { //looping while repeat value is true
+			try { //do a below contents
+				System.out.println("Enter an Integer : "); //print some message to get num value
+				num = keyboard.nextInt(); //num value gets from user
+				bound.recieve(num); //call a method recieve with parameter
 			}
-			catch(ArrayIndexOutOfBoundsException e) {
-				System.out.println("Invalid array index access!");
-				repeat = false;
+			catch(ArrayIndexOutOfBoundsException e) { //if any error which is in ArrayIndexOutOfBoundsException exists 
+				System.out.println("Invalid array index access!"); //notice about that error situation
+				repeat = false; //change repeat value to false
 			}
 		}
-		keyboard.close();
+		keyboard.close(); //close keyboard
 	}
 
 }
